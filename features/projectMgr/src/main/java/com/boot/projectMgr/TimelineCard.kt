dee669041cdb89f3600e532e05b3e358794d1833
Wasin Passornpakorn <pp.wasin@gmail.com>
2020-10-24 19:01:42 +0700

    change timeline implementation to custom layout

--- features/projectMgr/src/main/java/com/boot/projectMgr/TimelineCard.kt
+++ features/projectMgr/src/main/java/com/boot/projectMgr/TimelineCard.kt
@@ -7,61 +7,74 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AttachFile
+import androidx.compose.material.icons.filled.Attachment
 import androidx.compose.material.icons.filled.ChatBubbleOutline
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Layout
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.drawBehind
 import androidx.compose.ui.geometry.toRect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 
-val linePosition = 60.dp
+val linePosition = 80.dp
 
 fun Modifier.drawLine() = this.drawBehind {
     drawLine(Color.Black, start = size.toRect().topRight, end = size.toRect().bottomRight, 4f)
 }
 
 @Composable
-fun LineContainer(
-    content: @Composable () -> Unit,
+fun TimelineRow(
+    leftContent: @Composable () -> Unit,
+    rightContent: @Composable () -> Unit,
 ) {
-    Row(
-        Modifier
-            .padding(end = 16.dp)
-            .background(Color.Red)
-//            .layout { measurable, constraints ->
-//                val place = measurable.measure(constraints)
-//                layout()
-//            }
-            .drawLine()
-            .width(linePosition)
-            .fillMaxHeight(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center
-    ) {
-        content()
+    Layout(children = {
+        Row(
+            Modifier
+                .padding(end = 16.dp)
+                .drawLine(),
+            verticalAlignment = Alignment.CenterVertically
+        ) { leftContent() }
+        Row {
+            rightContent()
+        }
+    }) { (left, right), constraint ->
+        val leftWidth = linePosition.toIntPx()
+        val placeRight = right.measure(
+            Constraints(
+                minWidth = (constraint.minWidth - leftWidth).coerceAtLeast(0),
+                maxWidth = (constraint.maxWidth - leftWidth).coerceAtLeast(0),
+                minHeight = constraint.minHeight,
+                maxHeight = constraint.maxHeight)
+        )
+
+        val placeLeft = left.measure(
+            Constraints.fixed(width = leftWidth, height = placeRight.height)
+        )
+        layout(placeLeft.width + placeRight.width, placeRight.height) {
+            placeLeft.placeRelative(0, 0)
+            placeRight.placeRelative(leftWidth, 0)
+        }
     }
 }
 
+
 @Composable
 fun TimelineHeader() {
-    Row {
-        LineContainer {
-            Text("Data")
-        }
+    TimelineRow(
+        leftContent = { Text("Data") }
+    ) {
         Text("Tasks")
         Text("Show in days")
     }
+
 }
 
 @Composable
 fun TimelineTask(task: Task) {
-    Row {
-        LineContainer {
-            Text(task.timeCode)
-        }
+    TimelineRow(leftContent = { Text(task.timeCode) }) {
         Column(
             Modifier
                 .padding(vertical = 8.dp)
@@ -92,12 +105,13 @@ fun TimelineTask(task: Task) {
                 }
                 IconButton(onClick = {}) {
                     Row {
-                        Icon(Icons.Default.AttachFile)
+                        Icon(Icons.Default.Attachment)
                         Text(task.attachmentCount.toString())
                     }
                 }
                 Spacer(Modifier.weight(1f))
                 Text("N\u00B0 ${task.id}")
+                Spacer(Modifier.width(2.dp))
                 AvatarList(users = task.assignees, onUserClick = {}, size = 32.dp)
             }
         }
