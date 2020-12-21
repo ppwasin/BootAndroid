4a073f7975077b891ef0d6eab7c4a5a8d6eba0b7
ppwasin <pp.wasin@gmail.com>
2020-12-21 10:20:39 +0700

    revert back, the plyground plugin reuse code wrongly

--- playground/ComposePagingApp/build.gradle.kts
+++ playground/ComposePagingApp/build.gradle.kts
@@ -1,5 +1,37 @@
-apply<plugin.PlaygroundPlugin>()
+apply<plugin.JetpackComposePlugin>()
+apply<plugin.Junit5Plugin>()
 
-configure<plugin.PlaygroundPluginExtension> {
-    appId = "com.boot.pagingcompose"
+plugins {
+    moduleApp()
+    kotlinAndroid()
+}
+
+android {
+    configureApp("com.boot.pagingcompose")
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.txt")
+        }
+    }
+
+}
+
+dependencies {
+    implementation(fileTree(mapOf("include" to listOf("*.jar"), "dir" to "libs")))
+    implementation(Libs.timber)
+    implementation(Libs.kotlinStd)
+    implementation(Libs.coroutineCore)
+    implementation(Libs.coroutineAndroid)
+    implementation(Libs.appCompat)
+    implementation(Libs.googleMaterial)
+    implementation(Libs.constraintLayout)
+    implementation(Libs.playCore)
+
+    implementation(Libs.navDynamicFeature)
+    implementation(Libs.navFragment)
+    implementation(Libs.navKtx)
+
+    testImplementation(Libs.jUnit)
+    androidTestImplementation(Libs.androidTest)
 }
--- playground/ParallaxApp/build.gradle.kts
+++ playground/ParallaxApp/build.gradle.kts
@@ -1,5 +1,34 @@
-apply<plugin.PlaygroundPlugin>()
+apply<plugin.JetpackComposePlugin>()
+apply<plugin.Junit5Plugin>()
 
-configure<plugin.PlaygroundPluginExtension> {
-    appId = "com.boot.parallax"
+plugins {
+    moduleApp()
+    kotlinAndroid()
+}
+
+android {
+    configureApp("com.boot.parallax")
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.txt")
+        }
+    }
+
+}
+
+dependencies {
+    implementation(fileTree(mapOf("include" to listOf("*.jar"), "dir" to "libs")))
+    implementation(Libs.timber)
+    implementation(Libs.kotlinStd)
+    implementation(Libs.coroutineCore)
+    implementation(Libs.coroutineAndroid)
+    implementation(Libs.appCompat)
+    implementation(Libs.googleMaterial)
+    implementation(Libs.constraintLayout)
+
+    implementation(Libs.navFragment)
+    implementation(Libs.navKtx)
+
+    androidTestImplementation(Libs.androidTest)
 }
